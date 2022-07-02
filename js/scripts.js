d99897dfcc56535e1f05e299ed157f872931549c
Ularbek Akylbek uulu <ularbekakylbekuulu@MacBook-Air-ALTYNAI.local>
2022-07-02 19:54:05 +0500

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    let position = 0;
+    const slidesToshoW = 1;
+    const slidesToScrol = 1;
+    const slides = $(',slider');
+    const block = $(',slider_block');
+    const item = $(',slider_item');
+    const btnPrev = $(',slider_button_next');
+    const btnNext = $(',slider_button_prev');
+    const itemWidth = slider.width() / slidesToshoW;
+
+});
+slider
+slider_block
+slider_item
